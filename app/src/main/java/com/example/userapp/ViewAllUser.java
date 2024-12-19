9b9ff44e07cb8e76b17483aa76115c4e6efa0e79
AksharaaRaghu <akshararaghu2004@gmail.com>
2024-12-19 13:34:56 +0530

    view all done

---
+++ app/src/main/java/com/example/userapp/ViewAllUser.java
@@ -0,0 +1,26 @@
+package com.example.userapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ViewAllUser extends AppCompatActivity {
+
+    TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_view_all_user);
+
+        t1=(TextView) findViewById(R.id.abc);
+
+
+
+    }
+}
